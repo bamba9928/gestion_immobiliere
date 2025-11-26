d02d187444ac10af52b421ae4ba387a3dfc6f801
bamba9928 <bigrip2016@gmail.com>
2025-11-26 17:18:49 +0000

    first

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,6 +1,24 @@
 from django.urls import path
 from . import views
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from apps.core.views import dashboard
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="login.html",
+            redirect_authenticated_user=True,
+        ),
+        name="login",
+    ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(),
+        name="logout",
+    ),
+    path('document/quittance/<int:loyer_id>/', views.download_quittance, name='download_quittance'),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -3,9 +3,14 @@ from datetime import date
 from django.contrib.auth.decorators import login_required
 from django.db.models import Sum
 from django.shortcuts import render
+import weasyprint
+from django.utils import timezone
 
 from .models import Bail, Bien, Loyer
-
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from django.core.exceptions import PermissionDenied
 
 @login_required
 def dashboard(request):
@@ -44,3 +49,32 @@ def dashboard(request):
         context['user_role'] = 'LOCATAIRE'
 
     return render(request, 'dashboard.html', context)
+
+
+def download_quittance(request, loyer_id):
+    # 1. Récupérer le loyer
+    loyer = get_object_or_404(Loyer, id=loyer_id)
+
+    # 2. Sécurité : Seul le locataire du bail ou un Admin peut télécharger
+    if not request.user.is_staff and loyer.bail.locataire != request.user:
+        raise PermissionDenied("Vous n'avez pas accès à ce document.")
+
+    # 3. Vérifier que c'est bien payé (Optionnel, mais logique pour une quittance)
+    if loyer.statut != 'PAYE':
+        return HttpResponse("Ce loyer n'est pas encore soldé. Impossible d'éditer la quittance.", status=400)
+
+    # 4. Générer le HTML
+    html_string = render_to_string('documents/quittance.html', {
+        'loyer': loyer,
+        'now': timezone.now()
+    })
+
+    # 5. Convertir en PDF
+    response = HttpResponse(content_type='application/pdf')
+    # attachment; filename=... pour télécharger, inline; pour afficher dans le navigateur
+    filename = f"Quittance_{loyer.periode_debut.strftime('%Y-%m')}_{loyer.bail.locataire.username}.pdf"
+    response['Content-Disposition'] = f'inline; filename="{filename}"'
+
+    weasyprint.HTML(string=html_string).write_pdf(response)
+
+    return response
